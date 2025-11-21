29282fe0f62036159633fdae6ea43b480c90e7a5
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-11-22 01:16:24 +0900

    Update scoreModel.js

--- models/scoreModel.js
+++ models/scoreModel.js
@@ -1,20 +1,28 @@
 const mongoose = require("mongoose");
 
 const scoreSchema = new mongoose.Schema({
-    userId:{
+    userId:{ //아이디(중복X)
+        type: String,
+        required: true,
+        unique: true,
+    },
+    nickname:{ //닉네임(중복O)
         type: String,
         required: true,
     },
-    score:{
+    score:{ //최고 점수
         type: Number,
+        required: true,
     },
 },
     {
-        timestamps: {
+        time: { //걸린 시간
             type: Number,
+            required: true,
         },
     },
+    {timestamps: true} //
 );
-//
+
 const Score = mongoose.model("Score",scoreSchema); //Score 모델 생성
 module.exports = Score;
