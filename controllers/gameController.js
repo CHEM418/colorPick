9e9ae4029a603b057bdc74f3c5716130b0c8acd6
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-11-22 00:46:13 +0900

    Update gameController.js

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,13 +1,9 @@
-//[중요] 오류 처리 => 어싱크 핸들러로 오류 던지고 errorhandler.js에서 처리
-//비동기 처리를 위해서 async, await 사용
+//아직
 const asyncHandler = require("express-async-handler");
-
 
 /* const ex = asyncHandler(async(req,res)=>{
     res.render("getAll"); ejs등록 예시
     res.render(ejs파일,{변수:전송 자료}) 변수로 값 사용
 }); */
-
-
 
 //module.exports={createUser};
