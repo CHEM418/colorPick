f83bdbf2d5600042a49733da7949996f25b9e6d8
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-11-22 03:19:01 +0900

    gameController.js 업데이트

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,9 +1,9 @@
 //아직
 const asyncHandler = require("express-async-handler");
 
-/* const ex = asyncHandler(async(req,res)=>{
+/* 예시 const ex = asyncHandler(async(req,res)=>{
     res.render("getAll"); ejs등록 예시
     res.render(ejs파일,{변수:전송 자료}) 변수로 값 사용
 }); */
 
-//module.exports={createUser};
+//module.exports={ex};
