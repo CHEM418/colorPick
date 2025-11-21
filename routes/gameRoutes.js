5f3b1c5811e105d80f35d32e35624a67bbe8417d
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-11-22 00:42:28 +0900

    gameRoutes

---
+++ routes/gameRoutes.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+//const User = require("../models/userModel");
+//const Score = require("../models/scoreModel");
+//const {createUser} = require("../controllers/gameController");
+
+//router.route("/home") 홈 화면
+
+//router.route("/game")
+
+//router.route("/game/result")
+
+//router.route("/game/ranking")
+
+//router.route("/game/result/share")
+
+
+
+
+
+
+module.exports = router;
