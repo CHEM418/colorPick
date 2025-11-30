db448fb8e194dbfd7aa2265c8fc9aae475b031d6
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-11-30 19:51:38 +0900

    라우트 업데이트 - 컨트롤러랑 연결

--- routes/gameRoutes.js
+++ routes/gameRoutes.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-//router.route("/home") 홈 화면
-
-//router.route("/game")
-
-//router.route("/game/result")
-
-//router.route("/game/ranking")
-
-//router.route("/game/result/share")
+//
+const {gameResult,gameRanking} = require("../controllers/gameController");
 
 
+//실제로는 @/game
+//router.route("/")
 
+
+//실제로는 @/game/result
+router.route("/result")
+    .post(gameResult);
+
+
+//실제로는 @/game/ranking
+router.route("/ranking")
+    .get(gameRanking);
+
+//실제로는 @/game/reult/share
+//router.route("/result/share")
 
 
 
