1a3ff6dca94174a80ccccd0c392b7c3bb942726d
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-11-30 19:50:17 +0900

    에러 처리 기능(에러핸들러)

---
+++ errorhandler.js
@@ -0,0 +1,6 @@
+const errorhandler = (err, req, res, next) => {
+        res.status(err.status || 500);
+        res.json({message: err.message || "서버 오류 발생",});
+};
+        
+module.exports = errorhandler;
