38d04b023363e934c2c9e3fcd0f0aa481697c043
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-12-06 18:48:27 +0900

    Add error logging to errorhandler

--- errorhandler.js
+++ errorhandler.js
@@ -1,4 +1,5 @@
 const errorhandler = (err, req, res, next) => {
+        console.error("오류 발생:", err);
         res.status(err.status || 500);
         res.json({message: err.message || "서버 오류 발생",});
 };
