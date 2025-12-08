cb354ef0a11f23b5f05bbce4ffe5f13bb4deda77
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-12-08 23:10:28 +0900

    수정

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const dbConnect = require("./config/dbConnect");
 const errorhandler = require("./errorhandler");
 const cron = require("node-cron");
 const Score = require('./models/scoreModel');
-const {gameResult} = require('./controllers/gameController');
+const {gameResult,gameStageResult} = require('./controllers/gameController');
 
 //app 서버 생성
 const app = express();
@@ -27,7 +27,8 @@ app.use(express.static("public")); //정적 파일의 경로 알려줌
 app.use("/", require("./routes/main")); //main파일 내 라우터가 미들웨어를 처리하도록 함
 
 //결과 저장 api
-app.post('/api/save-record', gameResult);
+app.post('/api/save-record', gameStageResult);
+app.post('/api/save-total-time',gameResult);
 
 
 //ejs엔진 사용 설정
@@ -42,12 +43,14 @@ app.use(errorhandler);
 
 //정기적으로 데이터 삭제(분/시/일/월/요일)
 cron.schedule('0 0 * * *',async()=>{ //매일 자정에 삭제
+
         try{
             await Score.deleteMany({});
             console.log(`점수 데이터가 초기화 되었습니다.`);
         } catch(err){ 
-            console.log(`점수 데이터가 초기화에 실패했니다.`,err);//에러 정보
+            console.log(`점수 데이터가 초기화에 실패했니다.`,err);
         }
+
 });
 
 //서버 대기
