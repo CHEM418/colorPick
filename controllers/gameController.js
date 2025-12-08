f8f6e1457992ad9864752fbbc3991c665bccfe4e
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-12-08 23:05:33 +0900

    컨트롤러 재수정

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -2,14 +2,29 @@ const asyncHandler = require("express-async-handler");
 const Score = require("../models/scoreModel");
 
 
+// [각 스테이지가 끝나면 결과 저장]
+const gameStageResult = asyncHandler(async(req, res)=>{
+    const{userId,username,time,stageId}=req.body ;
+    if(!userId||!username||!time||!stageId){
+        return res.status(400).json({message:"아이디, 닉네임, 시간, 스테이지 기록이 필요합니다."});
+    }
+    await Score.findOneAndUpdate(
+        {userId, username, stageId},
+        {$set:{time}},{upsert: true, new:true} //방금 기록 덮어 쓰기,없으면 추가
+    );
+    res.status(200).json({message:"최근 기록이 저장됐습니다."})
+});
+
+
+// [최고기록 저장]
 const gameResult = asyncHandler(async(req,res)=>{
     const {userId,username,time} = req.body; //프론트에서 보내준 값
 
-    if(!userId || !username){ //입력값
-        return res.status(400).json({message:"아이디와 닉네임을 입력해주세요."}); 
+    if(!userId || !username||!time){ //입력값
+        return res.status(400).json({message:"아이디, 닉네임, 최고 기록이 필요합니다."}); 
     }
 
-    let exists=await Score.findOne({userId,username});
+    let exists=await Score.findOne({userId,username,stageId:0});//0번은 최고 기록
     var newRecord=false; //신기록 여부
 
     if(exists){//기존 유저: 업데이트
@@ -22,23 +37,23 @@ const gameResult = asyncHandler(async(req,res)=>{
         const newUser = await Score.create({
             userId,
             username,
+            stageId:0,
             time
         });
         newRecord=true;
         exists=newUser;
+
     }
-    // 게임 결과 랭킹(최고 점수들 기준)
-    const playerRank = await Score.countDocuments({time:{$lt:time}})+1;
 
     res.json({
         success: true,
         username,
         time,
+        stageId:0,
         newRecord,
-        playerRank
     });
+    
 });
 
-module.exports={gameResult};
+module.exports={gameResult,gameStageResult};
 //오류 => 어싱크 핸들러로 오류 던지고 errorhandler.js에서 처리
-
