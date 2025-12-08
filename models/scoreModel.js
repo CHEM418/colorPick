1b36319950ea9e3489e801c74f60bf8ed2e56864
CHEM418 <105440395+CHEM418@users.noreply.github.com>
2025-12-08 23:05:56 +0900

    모델 재수정

--- models/scoreModel.js
+++ models/scoreModel.js
@@ -9,20 +9,18 @@ const scoreSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    time: { //걸린 시간(=최고 기록)
+    time: { //걸린 시간
             type: Number,
             required: true,
         },
-    stageId:{
+    stageId:{ //*stageId 0번의 time은 최고 기록 저장 공간
         type:Number,
     }
 },
     {timestamps: true});
 
-scoreSchema.index({userId:1,username:1,stageId:1},{unique:true});//닉넴,아이디,스테이지별로 기록
+scoreSchema.index({userId:1,username:1,stageId:1},{unique:true});//닉네임+id+스테이지 별로 기록
 
 const Score = mongoose.model("Score",scoreSchema);
 module.exports = Score;
-
-
 
